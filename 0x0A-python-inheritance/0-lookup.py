05bf133a4ed474158deca51396ea6ec0cb8ce03e
EdwinKamau2 <edwinkamau2@gmail.com>
2023-07-09 23:42:41 -0700

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""  this returns the list of available attributes
+and methods of an object """
+
+
+def lookup(obj):
+    """this returns the list of available attributes"""
+    return dir(obj)
